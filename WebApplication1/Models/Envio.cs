a778b5059998e5138f96b6db18c5a336060de49b
Tomas Azzaretti <tomiazza13@gmail.com>
2023-06-09 10:44:12 -0300

    primer commit

---
+++ WebApplication1/Models/Envio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public partial class Envio
+    {
+        public Envio()
+        {
+            Venta = new HashSet<Venta>();
+        }
+
+        public int Id { get; set; }
+        public string Destino { get; set; } = null!;
+        public string Origen { get; set; } = null!;
+
+        public virtual ICollection<Venta> Venta { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Venta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public partial class Venta
+    {
+        public int IdEnvio { get; set; }
+        public int IdUsuario { get; set; }
+        public int IdProducto { get; set; }
+        public int IdVenta { get; set; }
+
+        public virtual Envio IdEnvioNavigation { get; set; } = null!;
+        public virtual Producto IdProductoNavigation { get; set; } = null!;
+        public virtual User IdUsuarioNavigation { get; set; } = null!;
+    }
+}

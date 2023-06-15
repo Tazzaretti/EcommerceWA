b74ffb764cb8ee63e8c657ea9958432028c6e397
FacuQuevedo <106189553+FacuQuevedo@users.noreply.github.com>
2023-06-15 13:37:18 -0300

    Composite Root

---
+++ WebApplication1/CompositeRoot.cs
@@ -0,0 +1,18 @@
+﻿using Model.Services;
+using Service.Services;
+
+namespace WebApplication1
+{
+    public class CompositeRoot
+    {
+        public static void DependencyInjection(WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IEnvioService,EnvioService>();
+            builder.Services.AddScoped<IProductosService, ProductosService>();
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IVentaService, VentaService>();
+            builder.Services.AddScoped<IVarianteService, VarianteService>();
+
+        }
+    }
+}
